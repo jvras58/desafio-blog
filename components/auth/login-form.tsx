3686d8b49dcc424f6f342f5d6a54bbf3d4806355
jvras58 <jvras@cin.ufpe.br>
2025-01-10 10:12:26 -0300

    refactor: simplificar o formulário de login e remover código não utilizado

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -22,7 +22,6 @@ export default function LoginForm() {
 	const [isPending, startTransition] = useTransition();
 	const [error, setError] = useState<string>("");
 	const [success, setSuccess] = useState<string>("");
-	const [showOTPForm] = useState<boolean>(false);
 	const searchParams = useSearchParams();
 	const callbackError = searchParams
 		? searchParams.get("error") === "OAuthAccountNotLinked"
@@ -41,7 +40,7 @@ export default function LoginForm() {
 		startTransition(async () => {
 			try {
 				const resp: { error?: string; success?: string } | undefined = await login(values);
-	
+
 				if (!resp) {
 					console.info("Algo deu errado");
 					setSuccess("");
@@ -49,14 +48,13 @@ export default function LoginForm() {
 					return;
 				}
 
-
 				if (resp.error) {
 					setError(resp.error);
 					setSuccess("");
 					form.reset();
 					return;
 				}
-	
+
 				if (resp.success) {
 					console.info("Sucesso ao conectar");
 					setSuccess(resp.success);
@@ -65,7 +63,7 @@ export default function LoginForm() {
 					router.push(process.env.NEXT_PUBLIC_AUTH_LOGIN_REDIRECT || "/");
 					return;
 				}
-	
+
 				setError("Algo deu errado");
 				setSuccess("");
 				form.reset();
@@ -82,74 +80,62 @@ export default function LoginForm() {
 			<div className="space-y-4">
 				<Form {...form}>
 					<form onSubmit={form.handleSubmit(onSubmit)}>
-						{!showOTPForm && (
-							<div className="space-y-4">
-								<FormField
-									control={form.control}
-									name="email"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>{"E-mail"}</FormLabel>
-											<FormControl>
-												<Input
-													type="email"
-													placeholder="voce@provedor.com.br"
-													required
-													{...field}
-													disabled={isPending}
-												/>
-											</FormControl>
-											<FormDescription className="hidden">{"Seu e-mail."}</FormDescription>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="password"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>{"Senha"}</FormLabel>
-											<FormControl>
-												<div>
-													<Input type="password" placeholder="******" required {...field} disabled={isPending} />
-												</div>
-											</FormControl>
-											<FormDescription className="hidden">{"Seu e-mail."}</FormDescription>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								{callbackError && <AuthFormMessage type="error" message={callbackError} title="Erro" />}
-								{error && <AuthFormMessage type="error" message={error} title="Erro" />}
-								{success && <AuthFormMessage type="success" message={success} title="Sucesso" />}
-								<Button variant={"default"} className="w-full" disabled={isPending}>
-									<LoaderIcon className={!isPending ? "hidden" : "animate-spin mr-2"} />
-									<span>{"Conectar"}</span>
-								</Button>
-							</div>
-						)}
+						<div className="space-y-4">
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>{"E-mail"}</FormLabel>
+										<FormControl>
+											<Input
+												type="email"
+												placeholder="voce@provedor.com.br"
+												required
+												{...field}
+												disabled={isPending}
+											/>
+										</FormControl>
+										<FormDescription className="hidden">{"Seu e-mail."}</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>{"Senha"}</FormLabel>
+										<FormControl>
+											<div>
+												<Input type="password" placeholder="******" required {...field} disabled={isPending} />
+											</div>
+										</FormControl>
+										<FormDescription className="hidden">{"Sua senha."}</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							{callbackError && <AuthFormMessage type="error" message={callbackError} title="Erro" />}
+							{error && <AuthFormMessage type="error" message={error} title="Erro" />}
+							{success && <AuthFormMessage type="success" message={success} title="Sucesso" />}
+							<Button variant={"default"} className="w-full" disabled={isPending}>
+								<LoaderIcon className={!isPending ? "hidden" : "animate-spin mr-2"} />
+								<span>{"Conectar"}</span>
+							</Button>
+						</div>
 					</form>
 				</Form>
 
 				<Separator />
 
-				{!showOTPForm && (
-					<div className="mt-4 text-center text-sm">
-						{"Não tem uma conta?"}{" "}
-						<Link href="/auth/register" className="underline">
-							{"Cadastre-se"}
-						</Link>
-					</div>
-				)}
-				{showOTPForm && (
-					<div className="mt-4 text-center text-sm">
-						{"Conectar agora?"}{" "}
-						<Link href="/auth/login" className="underline">
-							{"Conectar"}
-						</Link>
-					</div>
-				)}
+				<div className="mt-4 text-center text-sm">
+					{"Não tem uma conta?"}{" "}
+					<Link href="/auth/register" className="underline">
+						{"Cadastre-se"}
+					</Link>
+				</div>
 			</div>
 		</AuthCard>
 	);
