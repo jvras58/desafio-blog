4d3a444b2236b933c62daf630869c055d595e30e
jvras <jonathas.vinicius@ufpe.br>
2025-01-13 18:30:14 -0300

    feat: adicionar metadados à página do dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,6 +8,12 @@ import {
 import { SidebarSettings } from "@/components/sidebar/SidebarSettings";
 import PainelLayout from "@/components/painel/painel-layout";
 import PlaceholderContent from "@/components/demo/placeholder-content";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Dashboard",  	
+	description: "Pagina de Dashboard",
+};
 
 export default function DashboardPage() {
   return (
