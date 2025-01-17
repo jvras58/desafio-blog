dc83e7837c592069ae9b4461a2832c9f4d9042a8
jvras <jvras@cin.ufpe.br>
2025-01-17 07:53:20 -0300

    feat: adicionar carregamento assíncrono ao formulário de login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,14 +1,17 @@
+import { Suspense } from "react";
 import LoginForm from "@/components/auth/login-form";
 import { ContentLayout } from "@/components/painel/content-layout";
 
 const Login = async () => {
     return (
         <ContentLayout title="Login" showUserNav={false}>
-        <div className="flex flex-col w-full min-h-full items-center justify-center">
-            <div className="mt-20">
-                <LoginForm />
+            <div className="flex flex-col w-full min-h-full items-center justify-center">
+                <div className="mt-20">
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <LoginForm />
+                    </Suspense>
+                </div>
             </div>
-        </div>
         </ContentLayout>
     );
 };
